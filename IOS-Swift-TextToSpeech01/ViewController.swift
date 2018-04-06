6387c6ec5cd3bb8ed2562f47100362b2d4af91bb
Pooya Hatami <pooya@node-1w7jr9qrok5tc6lktawwm6gf8.ipv6.telus.net>
2018-04-05 17:24:58 -0700

    Initial Commit

---
+++ IOS-Swift-TextToSpeech01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS-Swift-TextToSpeech01
+//
+//  Created by Pooya Hatami on 2018-04-05.
+//  Copyright © 2018 Pooya Hatami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
