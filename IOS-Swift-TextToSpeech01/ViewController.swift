d3e2d50dca2758cdd2ef3bb682c93448d0214f90
Pooya Hatami <pooya@node-1w7jr9qrok5tc6lktawwm6gf8.ipv6.telus.net>
2018-04-05 20:13:20 -0700

    done :) !

--- IOS-Swift-TextToSpeech01/ViewController.swift
+++ IOS-Swift-TextToSpeech01/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textHere: UITextView!
+    @IBOutlet weak var segmnetCt: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func speechBtn(_ sender: UIButton) {
+        
+        var lang: String = "en-US"
+        switch segmnetCt.selectedSegmentIndex {
+        case 0:
+            lang = "en-US"
+             break;
+        case 1:
+            lang = "fr-FR"
+             break;
+        case 2:
+            lang = "de-DE"
+             break;
+        case 3:
+            lang = "es-ES"
+             break;
+        case 4:
+            lang = "it-IT"
+             break;
+        default:
+            lang = "en-US"
+            break;
+        }
+        
+        let uttrace = AVSpeechUtterance(string: textHere.text! )
+        uttrace.voice = AVSpeechSynthesisVoice(language: lang)
+        uttrace.rate = 0.5
+        
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(uttrace)
+        
+        
     }
-
-
+    
 }
 
